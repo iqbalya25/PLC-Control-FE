d82f754cb66e961a0ff79295d7babdc133b71eee
iqbalya25 <iqbalya25@gmail.com>
2024-08-19 10:42:47 +0700

    fixing api fetch

--- app/api/plccontrol.ts
+++ app/api/plccontrol.ts
@@ -1,15 +1,20 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
-const api = axios.create({
+const api: AxiosInstance = axios.create({
   baseURL: "https://plc-control-be-server-jvczhicy6q-uc.a.run.app/api/plc",
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // Add this if you need to send cookies with your requests
 });
 
-export const getLampStatus = async () => {
+interface LampStatus {
+  isOn: boolean;
+}
+
+export const getLampStatus = async (): Promise<LampStatus> => {
   try {
-    const response = await api.get("/status");
+    const response = await api.get<{ status: string }>("/status");
     console.log("Lamp status response:", response.data);
     return { isOn: response.data.status === "ON" };
   } catch (error) {
@@ -18,9 +23,9 @@ export const getLampStatus = async () => {
   }
 };
 
-export const sendCommand = async (command: "ON" | "OFF") => {
+export const sendCommand = async (command: "ON" | "OFF"): Promise<string> => {
   try {
-    const response = await api.post("/command", { command });
+    const response = await api.post<string>("/command", { command });
     return response.data;
   } catch (error) {
     console.error("Error sending command:", error);
